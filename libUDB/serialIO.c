6790e5ccaa4c3e2d2ed6927678252c13fc84b444
Kees Guijt <kees.guijt@upcmail.nl>
2018-06-16 10:20:11 +0200

    redo serial3 after update of source

--- libUDB/serialIO.c
+++ libUDB/serialIO.c
@@ -265,3 +265,117 @@ void __attribute__((__interrupt__, __no_auto_psv__)) _U2RXInterrupt(void)
 	interrupt_restore_corcon;
 	unset_ipl_on_output_pin;
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Serial 3
+
+static int16_callback_fptr_t serial3_callback_get_byte_to_send = NULL;
+static callback_uint8_fptr_t serial3_callback_received_byte = NULL;
+
+void udb_init_USART3(int16_callback_fptr_t tx_fptr, callback_uint8_fptr_t rx_fptr)
+{
+	serial3_callback_get_byte_to_send = tx_fptr;
+	serial3_callback_received_byte = rx_fptr;
+
+	// configure U3MODE
+	U3MODEbits.UARTEN = 0;      // Bit15 TX, RX DISABLED, ENABLE at end of func
+	//                          // Bit14
+	U3MODEbits.USIDL = 0;       // Bit13 Continue in Idle
+	U3MODEbits.IREN = 0;        // Bit12 No IR translation
+	U3MODEbits.RTSMD = 0;       // Bit11 Simplex Mode
+	//                          // Bit10
+	U3MODEbits.UEN = 0;         // Bits8,9 TX,RX enabled, CTS,RTS not
+	U3MODEbits.WAKE = 0;        // Bit7 No Wake up (since we don't sleep here)
+	U3MODEbits.LPBACK = 0;      // Bit6 No Loop Back
+	U3MODEbits.ABAUD = 0;       // Bit5 No Autobaud (would require sending '55')
+	U3MODEbits.URXINV = 0;      // Bit4 IdleState = 1  (for dsPIC)
+	U3MODEbits.BRGH = 1;        // Bit3 4 clocks per bit period
+	U3MODEbits.PDSEL = 0;       // Bits1,2 8bit, No Parity
+	U3MODEbits.STSEL = 0;       // Bit0 One Stop Bit
+
+	// Load all values in for U3STA SFR
+	U3STAbits.UTXISEL1 = 0;     //Bit15 Int when Char is transferred (1/2 config!)
+	U3STAbits.UTXINV = 1;       //Bit14 FrSky needs inverted output
+	U3STAbits.UTXISEL0 = 1;     //Bit13 Other half of Bit15
+	//                          //Bit12
+	U3STAbits.UTXBRK = 0;       //Bit11 Disabled
+	//U3STAbits.UTXEN = 1;        //Bit10 TX pins controlled by periph (handled below)
+	//U3STAbits.UTXBF = 0;        //Bit9 *Read Only Bit*
+	//U3STAbits.TRMT = 0;         //Bit8 *Read Only bit*
+	U3STAbits.URXISEL = 0;      //Bits6,7 Int. on character recieved
+	U3STAbits.ADDEN = 0;        //Bit5 Address Detect Disabled
+	//U3STAbits.RIDLE = 0;        //Bit4 *Read Only Bit*
+	//U3STAbits.PERR = 0;         //Bit3 *Read Only Bit*
+	//U3STAbits.FERR = 0;         //Bit2 *Read Only Bit*
+	U3STAbits.OERR = 0;         //Bit1 *Read Only Bit*
+	//U3STAbits.URXDA = 0;        //Bit0 *Read Only Bit*
+
+	_U3TXIP = INT_PRI_U3TX;     // Mid Range Interrupt Priority level, no urgent reason
+	_U3RXIP = INT_PRI_U3RX;     // Mid Range Interrupt Priority level, no urgent reason
+
+	_U3TXIF = 0;                // Clear the Transmit Interrupt Flag
+	_U3TXIE = 1;                // Enable Transmit Interrupts
+	_U3RXIF = 0;                // Clear the Receive Interrupt Flag
+	_U3RXIE = 1;                // Enable Receive Interrupts
+
+	U3MODEbits.UARTEN = 1;      // And turn the peripheral on
+	U3STAbits.UTXEN = 1;
+}
+
+void udb_serial3_set_rate(int32_t rate)
+{
+	U3BRG = UDB_BAUD(rate);
+}
+
+boolean udb_serial3_check_rate(int32_t rate)
+{
+	return (U3BRG == UDB_BAUD(rate));
+}
+
+void udb_serial3_start_sending_data(void)
+{
+	_U3TXIF = 1; // fire the tx interrupt
+}
+
+void __attribute__((__interrupt__, __no_auto_psv__)) _U3TXInterrupt(void)
+{
+	_U3TXIF = 0; // clear the interrupt
+	indicate_loading_inter;
+	set_ipl_on_output_pin;
+	interrupt_save_set_corcon;
+
+//	int16_t txchar = udb_serial_callback_get_byte_to_send();
+	int16_t txchar = -1;
+	if (serial3_callback_get_byte_to_send)
+	{
+		txchar = serial3_callback_get_byte_to_send();
+	}
+	if (txchar != -1)
+	{
+		U3TXREG = (uint8_t)txchar;
+	}
+	interrupt_restore_corcon;
+	unset_ipl_on_output_pin;
+}
+
+void __attribute__((__interrupt__, __no_auto_psv__)) _U3RXInterrupt(void)
+{
+	_U3RXIF = 0; // clear the interrupt
+	indicate_loading_inter;
+	set_ipl_on_output_pin;
+	interrupt_save_set_corcon;
+
+	while (U3STAbits.URXDA)
+	{
+		uint8_t rxchar = U3RXREG;
+//		udb_serial_callback_received_byte(rxchar);
+		if (serial3_callback_received_byte)
+		{
+			serial3_callback_received_byte(rxchar);
+		}
+	}
+	U3STAbits.OERR = 0;
+	interrupt_restore_corcon;
+	unset_ipl_on_output_pin;
+}
