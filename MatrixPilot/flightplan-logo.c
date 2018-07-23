b9cbdd75182c9a5fd3b36157b16339a222cb315e
Kees Guijt <kees.guijt@upcmail.nl>
2018-07-23 19:19:21 +0200

    go back to land 1

--- MatrixPilot/flightplan-logo.c
+++ MatrixPilot/flightplan-logo.c
@@ -1405,17 +1405,19 @@ static boolean process_one_instruction(struct logoInstructionDef instr)
 */
 
 						//land_1
-						/*
+						/**/
 						turtleLocations[currentTurtle].x._.W0 = 0;
 						turtleLocations[currentTurtle].x._.W1 = 45;
 						turtleLocations[currentTurtle].y._.W0 = 0;
 						turtleLocations[currentTurtle].y._.W1 = -39;
-						*/
+						/**/
 						//land_2
+						/*
 						turtleLocations[currentTurtle].x._.W0 = 0;
 						turtleLocations[currentTurtle].x._.W1 = -123;
 						turtleLocations[currentTurtle].y._.W0 = 0;
 						turtleLocations[currentTurtle].y._.W1 = +25;
+						*/
 						//land_3
 						/*
 						turtleLocations[currentTurtle].x._.W0 = 0;
